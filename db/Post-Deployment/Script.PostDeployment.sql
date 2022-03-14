bbdd876b20c73be59aab587f7552484592275ec0
Michael Griehm <griehmmj@gmail.com>
2022-03-14 18:06:18 -0400

    Fixed post deployment script

--- db/Post-Deployment/Script.PostDeployment.sql
+++ db/Post-Deployment/Script.PostDeployment.sql
@@ -1,10 +1,12 @@
 -- This file contains SQL statements that will be executed after the build script.
 IF NOT EXISTS (select * from sys.database_principals where name = 'nonprivileged')
-BEGIN
-    CREATE USER nonprivileged WITHOUT LOGIN
-    GRANT SELECT ON [Users] TO nonprivileged
-    GRANT SELECT ON [CryptoAlerts] TO nonprivileged
-END
+CREATE USER nonprivileged WITHOUT LOGIN
+GO
+
+GRANT SELECT ON [Users] TO nonprivileged
+GO
+
+GRANT SELECT ON [CryptoAlerts] TO nonprivileged
 GO
 
 IF NOT EXISTS (
@@ -14,7 +16,7 @@ INSERT INTO [dbo].[Users] (Email)
 VALUES ('griehmmj@gmail.com')
 GO
 
-DECLARE @UserId BIGINT;
+DECLARE @ AS BIGINT = 0;
 SELECT @UserId = [Id] FROM [dbo].[Users] WHERE [Email] = 'griehmmj@gmail.com';
 
 IF NOT EXISTS (
@@ -23,6 +25,9 @@ IF NOT EXISTS (
 INSERT INTO [dbo].[CryptoAlerts] ([UserId],[Symbol],[Name],[High],[Low],[PurchasedPrice])
 VALUES (@UserId,'MATIC','Polygon',2.00,1.30,1.90)
 GO
+
+DECLARE @ AS BIGINT = 0;
+SELECT @UserId = [Id] FROM [dbo].[Users] WHERE [Email] = 'griehmmj@gmail.com';
 
 IF NOT EXISTS (
     SELECT * FROM [dbo].[CryptoAlerts] WHERE [UserId] = @UserId AND [Symbol] = 'LRC'
