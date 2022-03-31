96cb94e81a6e6b889abe78fe91b662303cb6349f
Michael Griehm <griehmmj@gmail.com>
2022-03-31 15:26:59 -0400

    Moved stream analytics to its own area, for the setting up of the analytics output to function, we need the function access key, we won't have that until the Function is built and deployed, so need to move the setting up of the stream analytics to the back of the pipeline

---
+++ iac/stream-analytics/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azuread = "~>2.16.0"
+  }
+
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+variable "app_name" {
+  default   = "asa"
+  type      = string
+  sensitive = false
+}
+
+variable "env" {
+  default   = "demo"
+  sensitive = false
+}
+
+variable "location" {
+  default   = "East US 2"
+  sensitive = false
+}
+
+variable "tags" {
+  type = map(string)
+
+  default = {
+    environment = "demo"
+    workload    = "crypto-analytics"
+  }
+}
+
+variable "alert-function-access-key" {
+  type      = string
+  sensitive = true
+}
+
+locals {
+  loc            = lower(replace(var.location, " ", ""))
+  a_name         = replace(var.app_name, "-", "")
+  fqrn           = "${var.app_name}-${var.env}-${local.loc}"
+  fqrn_no_dashes = "${local.a_name}-${var.env}-${local.loc}"
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = local.fqrn
+}
